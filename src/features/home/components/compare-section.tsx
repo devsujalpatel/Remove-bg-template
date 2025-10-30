847852bcd2dbafab7313492232490e4de9a82e57
devsujalpatel <sujal7455@gmail.com>
2025-10-30 23:08:27 +0530

    Compare section created not finished

--- src/features/home/components/compare-section.tsx
+++ src/features/home/components/compare-section.tsx
@@ -3,23 +3,60 @@ import Image from "next/image";
 
 export const CompareSection = () => {
   return (
-    <div className="grid grid-cols-2 bg-[#378EF2]">
-      <div>
-        <Image src="/tabs/graphics/grid.svg" alt="Grid Image" width={500} height={500} />
+    <div className="grid grid-cols-2 bg-[#378EF2] max-w-[96vw] mx-auto rounded-xl px-20 py-30">
+      <div className="flex justify-start">
+        <Image
+          src="/tabs/graphics/graphic-grid-layout.png"
+          alt="Grid Image"
+          width={700}
+          height={800}
+        />
       </div>
-      <div>
-        <h1>What Makes ABC Different?</h1>
-        <p>
+      <div className="w-[82%]">
+        <h1 className="text-6xl leading-tight font-bold text-white">
+          What Makes ABC Different?
+        </h1>
+        <p className="text-lg font-medium text-white my-5">
           Picking the right background remover makes all the difference. Our
           tool is designed to give you speed, accuracy, and flexibility so you
           can focus on creating, not editing
         </p>
-        <div className="flex flex-col">
-          <Button>Lightning-Fast Editing</Button>
-          <Button>Smart AI Precision</Button>
-          <Button>Unlimited Creative Options</Button>
+        <div className="space-y-5">
+          <Button
+            className="rounded-full py-6.5 text-lg px-5 text-neutral-600 flex items-center justify-between hover:bg-transparent hover:border-[#BFE831] ease transition-all duration-300"
+            variant="outline"
+          >
+            <img
+              src="/elements/green-tick.svg"
+              alt="Green tick"
+              className="mr-3 size-7"
+            />
+            Lightning-Fast Editing
+          </Button>
+          <Button
+            className="rounded-full py-6.5 text-lg px-5 text-neutral-600 flex items-center justify-between hover:bg-transparent hover:border-[#BFE831] ease transition-all duration-300"
+            variant="outline"
+          >
+            <img
+              src="/elements/green-tick.svg"
+              alt="Green tick"
+              className="mr-3 size-7"
+            />
+            Smart AI Precision
+          </Button>
+          <Button
+            className="rounded-full py-6.5 text-lg px-5 text-neutral-600 flex items-center justify-between hover:bg-transparent hover:border-[#BFE831] ease transition-all duration-300"
+            variant="outline"
+          >
+            <img
+              src="/elements/green-tick.svg"
+              alt="Green tick"
+              className="mr-3 size-7"
+            />
+            Unlimited Creative Options
+          </Button>
         </div>
-        <Button>Start Creating</Button>
+        <Button className="rounded-full mt-8 text-lg bg-[#BFE831] text-black py-7 px-6">Start Creating</Button>
       </div>
     </div>
   );
